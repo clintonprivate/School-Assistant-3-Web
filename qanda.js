aaa7c4926931c98e6e7d92e3aebd4a11807ab258
clintonprivate <152466284+clintonprivate@users.noreply.github.com>
2024-02-14 22:03:41 +0100

    Create qanda.js

---
+++ qanda.js
@@ -0,0 +1,24 @@
+<frage>Rezitieren Sie das Kalkulationsschema.</frage>
+<antwort>
+Kostenträgerstückrechnung
+Zuschlagskalkulation
+
+Kalkulationsschema
+
+Fertigungsmaterial (FM)
++ % Materialgemeinkosten (MGK)
+= Materialkosten (MK)
+Fertigungslöhne (FL)
++ % Fertigungsgemeinkosten (FGK)
+= Fertigungskosten (FK)
+= Herstellkosten (HK)
++ % Verwaltungsgemeinkosten (VwGK)
++ % Vertriebsgemeinkosten (VtGK)
+= Selbstkosten (SK)
++ % Gewinnzuschlag
+= Barverkaufspreis (BVP)
++ % Kundenskonto
+= Zielverkaufspreis (ZVP)
++ % Kundenrabatt
+= Listenverkaufspreis (LVP)
+</antwort>
